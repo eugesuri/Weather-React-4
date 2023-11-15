a4ead68200122879f34d0410403da884126b93a6
eugesuri <140730354+eugesuri@users.noreply.github.com>
2023-11-15 15:29:22 +0100

    changes in the structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,90 +1,83 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
+import { Vortex } from "react-loader-spinner";
 import "./Weather.css";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Zürich",
-    date: "Friday 10:00",
-    description: "Cloudy",
-    imgUrl: "//ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-    humidity: 80,
-    wind: 60,
-    actualdate: "Friday 27th October",
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ loading: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  return (
-    <div className="Weather">
-      <div className="weather-app">
-        <form className="mb-3">
+  function handleData(response) {
+    setWeatherData({
+      loading: true,
+      coordinates: response.data.coord,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      windspeed: response.data.wind.speed,
+    });
+  }
+
+  function search() {
+    let apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleData);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.loading) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
-                type="search"
-                placeholder="Type a city..."
+                type="text"
+                placeholder="Enter city"
+                autoFocus="on"
                 className="form-control"
-                autocomplete="off"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-primary w-100"
+                className="search-button btn btn-light btn-outline-scondary w-100"
               />
             </div>
           </div>
         </form>
-
-        <div className="overview">
-          <h1>
-            <strong> {weatherData.city} </strong>{" "}
-          </h1>
-          <ul>
-            <li>
-              Last updated: {weatherData.date}
-              <span></span>
-            </li>
-            <li> {weatherData.description}</li>
-          </ul>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <div className="d-flex weather-temperature">
-              <img src={weatherData.imgUrl} id="icon" />
-              <div>
-                <strong></strong>
-                <span className="units">
-                  {" "}
-                  12
-                  <a href="/"> °C</a> |<a href="/"> °F </a>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>
-                <i className="fa-solid fa-droplet"></i> Humidity:
-                <span> {weatherData.humidity} </span>%
-              </li>
-              <li>
-                <i className="fa-solid fa-wind"></i> Wind:
-                <span> {weatherData.wind}</span>km/h
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="weather-forecast">
-          <div className="row">
-            <div className="col-2">
-              <div className="weather-forecast-date"></div>
-              {weatherData.actualdate}
-            </div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
-    
-    </div>
-  );
+    );
+  } else {
+    search();
+
+    return (
+      <Vortex
+        visible={true}
+        height="120"
+        width="120"
+        ariaLabel="vortex-loading"
+        wrapperStyle={{}}
+        wrapperClass="vortex-wrapper"
+        colors={["red", "green", "blue", "yellow", "orange", "purple"]}
+      />
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 import App from "./App";
-import Weather from "./Weather";
+import reportWebVitals from "./reportWebVitals";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    <Weather />
-  </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+reportWebVitals();
